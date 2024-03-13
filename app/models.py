4ecba33332f02844ad347e5f6dd6a9602124609a
TheClockwork9438 <rogercf1901@gmail.com>
2024-03-13 15:51:54 -0400

    adding user model

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: str
+    nombre: str
+    edad: int
+    telefono: str
+    fecha_creacion: datetime
+
